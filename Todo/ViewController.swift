2b1f68559fcec6e9eae27a3a1094cb24777dc859
Egor Tkachenko <egortka@egors-imac.local>
2018-12-25 23:05:02 +0700

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Egor Tkachenko on 25/12/2018.
+//  Copyright © 2018 ET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
